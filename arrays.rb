b28874d7d880136e335c57edf3e1152941293d82
Candace Eckels <candace.eckels@gmail.com>
2022-03-09 13:57:39 -0700

    Array_inputs

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+# Arrays Practice
+
+names = ["Jim", "Slay", "Switch", "Ben"]
+
+ages = [18, 37, 78, 21]
+
+heights = [72.5, 85.6, 65.8, 92.3]
+
+verified_heights = true
+verified_heights # true
+
+verified_heights = false
+verified_heights # false
