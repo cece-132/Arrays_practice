ba57cd0396882b24665b492c4c8c676439b9848f
Candace Eckels <candace.eckels@gmail.com>
2022-03-09 15:07:50 -0700

    Variable_comments

--- arrays.rb
+++ arrays.rb
@@ -1,13 +1,17 @@
 # Arrays Practice
 
 names = ["Jim", "Slay", "Switch", "Ben"]
+# prints the last element in the array
+puts names.pop
 
 ages = [18, 37, 78, 21]
+# prints all the elements
+puts ages.push
 
 heights = [72.5, 85.6, 65.8, 92.3]
+# pulls the first element ageheight
+puts heights.shift
 
-verified_heights = true
-verified_heights # true
-
-verified_heights = false
-verified_heights # false
+verified_heights = [true, false, true, true]
+# prints the elements
+puts verified_heights.unshift
